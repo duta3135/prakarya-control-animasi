f8200b9c6f12e8b3a519d091e76eaa8d9b6aebad
duta3135 <dutasetyawan1012@gmail.com>
2022-02-25 12:37:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function play(){
+    document.querySelector('.square').style.animationPlayState = "running"
+}
+function pause(){
+    document.querySelector('.square').style.animationPlayState = "paused"
+}
